1280ea90e23304e213f697fecf177b0dd2203926
Roy Solberg <git@roysolberg.com>
2015-10-02 20:30:48 +0200

    Fixed up button.

--- app/src/main/java/com/roysolberg/android/developertools/ui/activity/ResourceQualifiersActivity.java
+++ app/src/main/java/com/roysolberg/android/developertools/ui/activity/ResourceQualifiersActivity.java
@@ -2,7 +2,9 @@ package com.roysolberg.android.developertools.ui.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.roysolberg.android.developertools.R;
 
@@ -15,6 +17,12 @@ public class ResourceQualifiersActivity extends FragmentActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getString(R.string.resource_qualifiers));
         toolbar.setNavigationIcon(R.drawable.tinted_arrow_left);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavUtils.navigateUpFromSameTask(ResourceQualifiersActivity.this);
+            }
+        });
     }
 
 }
